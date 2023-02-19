8159a09a19f34113129d0f249601b6aa5b875b91
Aashir Khan <kaashir9@gmail.com>
2023-02-19 13:13:39 +0500

    basic struct and dbconfigs

---
+++ notes-service/initializers/database.go
@@ -0,0 +1,24 @@
+package initializers
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+
+	dsn := os.Getenv("DB_URL")
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Fail to connect to database")
+	} else {
+		log.Fatal("DB: ", DB)
+	}
+
+}
---
+++ notes-service/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"notes/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:3000
+}
